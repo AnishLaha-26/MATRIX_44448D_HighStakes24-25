c3b2b7181b19e51eb0f3a3bdc721e07ba484fb1e
AnishLaha-26 <113976746+AnishLaha-26@users.noreply.github.com>
2024-12-01 11:01:54 +0700

    cahnges to intake and pneumatics
    working wall stake auton 15 sec

---
+++ .d/Otherway-dc.cpp.d
@@ -0,0 +1,1 @@
+bin/Otherway-dc.cpp.o: src/Otherway-dc.cpp
---
+++ src/Otherway-dc.cpp
@@ -0,0 +1,20 @@
+// void autonomous() {
+// //chassis.calibrate(); // calibrate sensors
+//     // Set the initial robot pose
+//     chassis.setPose(0, 0, 0);
+//     chassis.moveToPose(
+//         18,                 // x-coordinate
+//         -12,                 // y-coordinate
+//         -93,                // theta (heading in degrees)
+//         2000,               // timeout in milliseconds
+//         {.forwards = false, .horizontalDrift = 8, .lead = 0.6,  // Move backward
+// }       // Combined MoveToPoseParams
+//     );
+    
+//     chassis.moveToPoint(18, -9 ,2000, {.forwards = false}, false);
+
+//     // Use swingToPoint to face a target point
+//    // chassis.swingToPoint(21, 30, DriveSide::RIGHT, 1000);
+
+  
+// }
